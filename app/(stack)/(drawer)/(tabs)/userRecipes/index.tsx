8b266d66c75910a7b3781cac31a4cb3178970929
XGM12 <xg_mosaic@proton.me>
2026-05-10 17:05:06 +0200

    story: añadido al hook que hace la llamada a la API

--- app/(stack)/(drawer)/(tabs)/userRecipes/index.tsx
+++ app/(stack)/(drawer)/(tabs)/userRecipes/index.tsx
@@ -1,6 +1,13 @@
 import {Text, View} from "react-native";
+import {useUserStore} from "@/presentation/hooks/store/useStore";
+import {useRecipes} from "@/presentation/hooks/recipes/useRecipes";
 
 const UserRecipesScreen = () => {
+    const {user} = useUserStore();
+    const {queryUserRecipes} = useRecipes(user?.id);
+
+    const userRecipes = queryUserRecipes.data?.flat() ?? []
+
     return (
         <View>
             <Text>UserRecipesScreen</Text>
