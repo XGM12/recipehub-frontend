405822db964f3a86aa4d2f783d85f1760d044a6f
XGM12 <xg_mosaic@proton.me>
2026-05-10 17:14:41 +0200

    fix: layout fixed

--- app/(stack)/(drawer)/(tabs)/_layout.tsx
+++ app/(stack)/(drawer)/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
-import { Ionicons } from '@expo/vector-icons'
-import { Tabs } from 'expo-router'
+import {Ionicons} from '@expo/vector-icons'
+import {Tabs} from 'expo-router'
 import React from 'react'
 
 const TabsLayout = () => {
@@ -13,7 +13,7 @@ const TabsLayout = () => {
                     title: 'Home',
                     tabBarShowLabel: false,
                     headerShown: false,
-                    tabBarIcon: ({ color }) => <Ionicons size={28} name="home-outline" color={color} />,
+                    tabBarIcon: ({color}) => <Ionicons size={28} name="home-outline" color={color}/>,
                 }}
             />
 
@@ -23,7 +23,27 @@ const TabsLayout = () => {
                     title: 'Búsqueda',
                     tabBarShowLabel: false,
                     headerShown: false,
-                    tabBarIcon: ({ color }) => <Ionicons size={28} name="search-outline" color={color} />,
+                    tabBarIcon: ({color}) => <Ionicons size={28} name="search-outline" color={color}/>,
+                }}
+            />
+
+            <Tabs.Screen
+                name="favourites/index"
+                options={{
+                    title: "Favoritos",
+                    tabBarShowLabel: false,
+                    headerShown: false,
+                    tabBarIcon: ({color}) => <Ionicons size={28} name="heart-outline" color={color}/>,
+                }}
+            />
+
+            <Tabs.Screen
+                name="userRecipes/index"
+                options={{
+                    title: "Mis recetas",
+                    tabBarShowLabel: false,
+                    headerShown: false,
+                    tabBarIcon: ({color}) => <Ionicons size={28} name="library-outline" color={color}/>
                 }}
             />
         </Tabs>
